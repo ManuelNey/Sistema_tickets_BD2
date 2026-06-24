0d4980135423ce55d01f53a368815fd7cbcdc30c
Constanza Sofia Blanco Zufiría <constanza.blanco@correo.ucu.edu.uy>
2026-06-24 18:04:18 -0300

    Restricciones de encuentros con admin al agregar o eliminar funcionarios

--- backend/Ticketing.API/Repositories/ITrabajaEnRepository.cs
+++ backend/Ticketing.API/Repositories/ITrabajaEnRepository.cs
@@ -5,6 +5,7 @@ namespace Ticketing.API.Repositories;
 public interface ITrabajaEnRepository
 {
     Task<IReadOnlyCollection<TrabajaEnDto>> GetByEncuentroAsync(int encuentroId);
-    Task<bool> CreateAsync(CrearTrabajaEnDto asignacion);
-    Task<bool> DeleteAsync(string funcionarioMail, int habilitaId);
+    Task<bool> CreateAsync(CrearTrabajaEnDto asignacion, int paisSedeId);
+
+    Task<bool> DeleteAsync(string funcionarioMail, int habilitaId, int paisSedeId);
 }
